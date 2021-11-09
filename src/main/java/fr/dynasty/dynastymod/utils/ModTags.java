a9b6873b7c4298839bcb78cc1b423b51af36d042
Ferfoui <kenji.oku@outlook.fr>
2021-11-09 20:50:10 +0100

    Update ModTags.java

--- src/main/java/fr/dynasty/dynastymod/utils/ModTags.java
+++ src/main/java/fr/dynasty/dynastymod/utils/ModTags.java
@@ -4,29 +4,37 @@ import fr.dynasty.dynastymod.DynastyMod;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.ITag;
+import net.minecraft.tags.ITag.INamedTag;
 import net.minecraft.tags.ItemTags;
 
 public class ModTags {
 
     public static class Items {
 
-        public static final ITag.INamedTag<Item> CELESTIAL_BLOCKS = tag("celestial_blocks");
-        public static final ITag.INamedTag<Item> INFUSER_ITEMS = tag("infuser_items");
-        public static final ITag.INamedTag<Item> PALM_LOGS = tag("palm_logs");
+        public static final INamedTag<Item> CELESTIAL_BLOCKS = tag("celestial_blocks");
+        public static final INamedTag<Item> INFUSER_ITEMS = tag("infuser_items");
+        public static final INamedTag<Item> PALM_LOGS = tag("palm_logs");
 
-        private static ITag.INamedTag<Item> tag(String name) {
+        private static INamedTag<Item> tag(String name) {
             return ItemTags.bind(DynastyMod.rl(name).toString());
+        }
+
+        private static INamedTag<Item> forgeTag(String name) {
+            return ItemTags.bind("forge:" + name);
         }
     }
 
     public static class Blocks {
 
-        public static final ITag.INamedTag<Block> CELESTIAL_BLOCKS = tag("celestial_blocks");
-        public static final ITag.INamedTag<Block> PALM_LOGS = tag("palm_logs");
+        public static final INamedTag<Block> CELESTIAL_BLOCKS = tag("celestial_blocks");
+        public static final INamedTag<Block> PALM_LOGS = tag("palm_logs");
 
-        private static ITag.INamedTag<Block> tag(String name) {
+        private static INamedTag<Block> tag(String name) {
             return BlockTags.bind(DynastyMod.rl(name).toString());
+        }
+
+        private static INamedTag<Block> forgeTag(String name) {
+            return BlockTags.bind("forge:" + name);
         }
     }
 }
