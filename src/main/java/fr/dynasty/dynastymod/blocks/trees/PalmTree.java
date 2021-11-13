bdc713992155a39f7f43757a0950fe7809ab3a9a
Ferfoui <kenji.oku@outlook.fr>
2021-11-13 16:34:02 +0100

    Update PalmTree.java

--- src/main/java/fr/dynasty/dynastymod/blocks/trees/PalmTree.java
+++ src/main/java/fr/dynasty/dynastymod/blocks/trees/PalmTree.java
@@ -11,7 +11,7 @@ import java.util.Random;
 public class PalmTree extends Tree {
     @Nullable
     @Override
-    protected ConfiguredFeature<BaseTreeFeatureConfig, ?> getConfiguredFeature(Random p_225546_1_, boolean p_225546_2_) {
+    protected ConfiguredFeature<BaseTreeFeatureConfig, ?> getConfiguredFeature(Random random, boolean bees) {
         return ModFeatures.PALM_TREE;
     }
 }
